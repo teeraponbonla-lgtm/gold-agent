f88f055a0347fb119a58be929994103d5e9c7e8b
teeraponbonla-lgtm <teerapon.bonla@gmail.com>
2026-06-07 02:27:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import yfinance as yf
+import feedparser
 from datetime import datetime, timedelta
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
@@ -13,6 +14,16 @@ current = round(hist["Close"].iloc[-1], 2)
 previous = round(hist["Close"].iloc[-2], 2)
 
 change = round(current - previous, 2)
+# ===== NEWS =====
+
+feed = feedparser.parse(
+    "https://feeds.finance.yahoo.com/rss/2.0/headline?s=GC=F&region=US&lang=en-US"
+)
+
+news = []
+
+for item in feed.entries[:3]:
+    news.append("• " + item.title)
 
 if change > 0:
     trend = "Bullish 📈"
@@ -33,6 +44,10 @@ Gold Price: {current}
 Change: {change}
 
 Trend: {trend}
+
+📰 ข่าวล่าสุด
+
+{chr(10).join(news)}
 
 ⚠️ ใช้เป็นข้อมูลประกอบการตัดสินใจ
 """
